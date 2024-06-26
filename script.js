e94e4d6f80e538fd9470217a055e100e55e11bcd
ThiagoSQA <163041242+ThiagoSQA@users.noreply.github.com>
2024-06-26 10:55:43 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -2,3 +2,8 @@ window.onbeforeunload = function() {
 
 return "vocçê tem Certeza que deseja sair?"
 };
+
+document.querySelector('form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Mensagem enviada!');
+});
